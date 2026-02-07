2ab768d9b87873945f31eb8a72b8ac1a4a30a522
vaisest <4550061+vaisest@users.noreply.github.com>
2026-02-07 23:16:37 +0200

    built files

--- src/components/profile/ProfileContext.tsx
+++
@@ -1,8 +0,0 @@
-import {createContext} from "react";
-import {selectedProfile} from "../../utils/LocalStorage";
-
-export const ProfileContext = createContext({
-  globalProfile: selectedProfile(),
-  setGlobalProfile: (profile: string) => {
-  }
-});
